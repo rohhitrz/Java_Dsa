9026d71c0dd511c406701baeea49743f7857f3c9
Rohit Raj <96881293+rohhitrz@users.noreply.github.com>
2023-07-02 22:35:11 +0530

    Add files via upload

---
+++ Mathematics/AllDIvisorOfNum.java
@@ -0,0 +1,17 @@
+package Mathematics;
+
+public class AllDIvisorOfNum {
+
+    public static void divisor(int num){
+        for (int i = 1; i <=num ; i++) {
+            if(num%i==0){
+                System.out.println(i);
+            }
+
+        }
+    }
+
+    public static void main(String[] args) {
+        divisor(100);
+    }
+}
