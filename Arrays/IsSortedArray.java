ca4fdc6547c59df9944e112748bcbd0eb2b584fd
Rohit Raj <96881293+rohhitrz@users.noreply.github.com>
2023-07-07 22:05:41 +0530

    Add files via upload

---
+++ Arrays/IsSortedArray.java
@@ -0,0 +1,19 @@
+package Arrays;
+
+public class IsSortedArray {
+    static boolean sorted(int arr[]){
+        for (int i = 1; i <arr.length; i++) {
+            if (arr[i] < arr[i - 1]) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    public static void main(String[] args) {
+        int arr[]={1,2,3,4,5};
+        boolean ans = sorted(arr);
+        System.out.println(ans);
+    }
+}
