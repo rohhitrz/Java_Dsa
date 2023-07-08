ebe92b25c4b625a19be161cd76daa0771f14a36c
Rohit Raj <96881293+rohhitrz@users.noreply.github.com>
2023-07-08 22:03:05 +0530

    Add files via upload

---
+++ Strings/PrintAllSubstring.java
@@ -0,0 +1,27 @@
+package Strings;
+
+public class PrintAllSubstring {
+
+    public static void printSubstrings(String str) {
+
+
+        for(int start=0; start<str.length(); start++)
+        {
+            for(int end=start; end<str.length(); end++)
+            {
+                System.out.println(str.substring(start,end+1));
+            }
+        }
+
+
+    }
+
+
+    public static void main(String[] args) {
+        String str = "abc";
+        printSubstrings(str);
+        //System.out.println(str.charAt(0)+str.charAt(1));
+
+    }
+}
+
