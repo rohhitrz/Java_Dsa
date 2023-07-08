03e09b3a7e2350692d9edfc0937a53b3d872232d
Rohit Raj <96881293+rohhitrz@users.noreply.github.com>
2023-07-08 22:03:23 +0530

    Add files via upload

---
+++ Strings/ReverseString.java
@@ -0,0 +1,22 @@
+package Strings;
+
+import java.util.Scanner;
+
+public class ReverseString {
+
+    static String reverse(String str){
+        String rev="";
+        for (int i = str.length()-1; i>=0; i--) {
+            rev+=str.charAt(i);
+        }
+        return rev;
+    }
+
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        String str= s.nextLine();
+        String res=reverse(str);
+        System.out.println(res);
+
+    }
+}
