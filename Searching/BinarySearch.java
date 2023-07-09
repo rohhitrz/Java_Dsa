d07bd2963400867eccb6bcc3077f842f89342bef
Rohit Raj <96881293+rohhitrz@users.noreply.github.com>
2023-07-09 18:44:39 +0530

    Add files via upload

---
+++ Searching/BinarySearch.java
@@ -0,0 +1,31 @@
+package Searching;
+
+public class BinarySearch {
+
+    static int BS(int arr[], int target){
+        int start=0;
+        int end=arr.length-1;
+
+        while (start<=end) {
+            int mid = (start + end) / 2;
+
+
+            if (arr[mid] == target) {
+                return mid;
+            }
+            else if (target > arr[mid]) {
+                start = mid + 1;
+            }
+            else{
+                end=mid-1;
+            }
+        }
+        return -1;
+    }
+
+    public static void main(String[] args) {
+        int arr[]= {10,20,30,40,50,60};
+        int res=BS(arr,10);
+        System.out.println(res);
+    }
+}
