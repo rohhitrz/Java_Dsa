64a9e5746823214941ca2a61d3c9931494651b68
Rohit Raj <96881293+rohhitrz@users.noreply.github.com>
2023-07-16 17:38:58 +0530

    Add files via upload

---
+++ LinkedList/CreateLL.java
@@ -0,0 +1,23 @@
+package LinkedList;
+
+public class CreateLL {
+    static class Node{
+        int data;
+        Node next;
+
+        Node(int x){
+            data=x;
+            next=null;
+        }
+    }
+
+    public static void main(String[] args) {
+
+        Node head= new Node(10);
+        Node second = new Node(20);
+        Node third = new Node(30);
+        head.next=second;
+        second.next=third;
+        System.out.println(head.data+"->"+ second.data+"->"+third.data); //10->20->30
+    }
+}
