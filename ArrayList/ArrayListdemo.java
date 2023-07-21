c54ec7d1cb87f43a341a5bb096e03f293c9ca53a
Rohit Raj <96881293+rohhitrz@users.noreply.github.com>
2023-07-21 15:52:18 +0530

    Add files via upload

---
+++ ArrayList/ArrayListdemo.java
@@ -0,0 +1,41 @@
+package ArrayList;
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class ArrayListdemo {
+
+    public static void main(String[] args) {
+
+
+        ArrayList<Integer> list1 = new ArrayList<>(10);
+        System.out.println(list1.size());
+        list1.add(10);
+        list1.add(15);
+        list1.add(20);
+        System.out.println(list1.size());
+        System.out.println(list1.get(2));
+
+        list1.add(1,50);
+
+        System.out.println("Printing All the elements: ");
+
+        for (int i = 0; i <list1.size() ; i++) {
+            System.out.print(" "+list1.get(i)+" ");
+
+        }
+        System.out.println();
+
+        //use remove
+        list1.remove(1);
+        System.out.println("Elements after removing the element at first Index: ");
+        for (int i = 0; i <list1.size() ; i++) {
+            System.out.print(" "+list1.get(i)+" ");
+
+        }
+
+
+
+
+
+    }
+}
