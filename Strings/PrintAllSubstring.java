f9d6b631154af754b2e8a1f36165be88e9106b1c
Rohit <96881293+rohhitrz@users.noreply.github.com>
2025-08-24 00:29:28 +0530

    Update PrintAllSubstring.java

--- Strings/PrintAllSubstring.java
+++ Strings/PrintAllSubstring.java
@@ -3,8 +3,7 @@ package Strings;
 public class PrintAllSubstring {
 
     public static void printSubstrings(String str) {
-
-
+//removing extra spaces
         for(int start=0; start<str.length(); start++)
         {
             for(int end=start; end<str.length(); end++)
@@ -12,10 +11,7 @@ public class PrintAllSubstring {
                 System.out.println(str.substring(start,end+1));
             }
         }
-
-
     }
-
 
     public static void main(String[] args) {
         String str = "abc";
