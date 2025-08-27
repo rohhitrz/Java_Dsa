5dc8de7b9243176f16c1d78c188823f1c0a457af
Rohit <96881293+rohhitrz@users.noreply.github.com>
2025-08-27 12:07:58 +0530

    Update AllDIvisorOfNum.java

--- Mathematics/AllDIvisorOfNum.java
+++ Mathematics/AllDIvisorOfNum.java
@@ -1,13 +1,11 @@
 package Mathematics;
 // printing all divisors of a number
 public class AllDIvisorOfNum {
-
     public static void divisor(int num){
         for (int i = 1; i <=num ; i++) {
             if(num%i==0){
                 System.out.println(i);
             }
-
         }
     }
 
